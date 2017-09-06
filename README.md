401db4cf00a3d7dd0d1eb34f992f5c9834abcb4a
Daniela Trulls <danielatrulls01@gmail.com>
2017-09-05 20:27:14 -0700

    Add initial files.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Redux project 2
+
+
+#### Clone from Github
+
+```
+> git clone
+> cd redux-project-2
+> npm install
+> npm start
+```
---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Fist line of code</div>
+    );
+  }
+}
